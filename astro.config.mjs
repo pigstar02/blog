cc659b921e05e45df96269499db8cc1c20a65498
pigstar02 <1910156852@qq.com>
2023-10-19 19:27:27 +0800

    删多了

--- astro.config.mjs
+++ astro.config.mjs
@@ -101,6 +101,7 @@ function pipeline() {
 
     () => (tree) => {
       for (let i = 0; i < tree.children.length; i++) {
+        let node = tree.children[i];
         if (node.type === "element" && ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', "ul", "ol","blockquote"].includes(node.tagName)) {
 
           let next = tree.children[i + 1];
